1dd73436991858cf0a7a745871c3eabc71d768bf
Snail <zxb340825@163.com>
2021-04-05 17:52:57 +0800

    Site updated: 2021-04-05 17:52:51

--- js/snow.js
+++ js/snow.js
@@ -1,3 +1,5 @@
+<script type="text/javascript" src="http://libs.baidu.com/jquery/1.8.3/jquery.js"></script>
+<script type="text/javascript" src="http://libs.baidu.com/jquery/1.8.3/jquery.min.js"></script>
 /*样式二*/
 /* 控制下雪 */
 function snowFall(snow) {
